88205e8f541ff1faf06bca97eb8c7cacb1eb5515
dlwsdqdws <dlwsdqdws@gmail.com>
2022-12-22 00:38:35 +0800

    add_map_and_walls

--- web/src/views/pk/PKIndexView.vue
+++ web/src/views/pk/PKIndexView.vue
@@ -1,13 +1,13 @@
 <template>
-    <ContentField>PK</ContentField>
+    <PlayGround /> 
 </template>
 
 <script>
-    import ContentField from "../../components/ContentField.vue";
+    import PlayGround from "../../components/PlayGround.vue";
 
     export default {
         components : {
-            ContentField
+            PlayGround
         }
     }
 </script>
