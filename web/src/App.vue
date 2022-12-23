3e20ee4b40f9ea2af3bf021b6a3ae694c3420f72
dlwsdqdws <dlwsdqdws@gmail.com>
2022-12-24 02:10:00 +0800

    API_login_info

--- web/src/App.vue
+++ web/src/App.vue
@@ -9,9 +9,42 @@
 
   import NavBar from './components/NavBar.vue';
 
+  import $ from "jquery";
+
   export default {
     components: {
       NavBar
+    },
+
+    setup(){
+      $.ajax({
+        url: "http://127.0.0.1:3000/user/account/token/",
+        type : "post",
+        data : {
+          username : 'test',
+          password : 'test',
+        },
+        success(resp){
+          console.log(resp);
+        },
+        error(resp){
+          console.log(resp);
+        }
+      }),
+
+      $.ajax({
+        url : "http://127.0.0.1:3000/user/account/info/",
+        type : "get",
+        headers : {
+          Authorization : "Bearer " + "eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiIzMDliOTVjNWQxZGU0NWE0OWZhOGZkNjE0NmYxZjQxNCIsInN1YiI6IjIiLCJpc3MiOiJzZyIsImlhdCI6MTY3MTgxNTA1NSwiZXhwIjoxNjczMDI0NjU1fQ.o46hvaowUISGmXEuf4QpZE5E13zkH0H_AM0G0gpCrJg"
+        },
+        success(resp){
+          console.log(resp);
+        },
+        error(resp){
+          console.log(resp);
+        }
+      })
     }
   }
 </script>
