c65c9880334875822bce49d8ddb5fd07c6a62d73
dlwsdqdws <dlwsdqdws@gmail.com>
2022-12-23 23:10:10 +0800

    add_mysql_and_login

--- backend/src/test/java/com/kob/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/kob/backend/BackendApplicationTests.java
@@ -2,12 +2,19 @@ package com.kob.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootTest
 class BackendApplicationTests {
 
     @Test
     void contextLoads() {
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        System.out.println(passwordEncoder.encode("ll"));
+        System.out.println(passwordEncoder.encode("test"));
+        System.out.println(passwordEncoder.encode("hapi"));
+//        System.out.println(passwordEncoder.matches("test","$2a$10$CG9hYPYyw7HUtpVElBCLx.gyIRvTjutY8NiizkWC1hEL6TthGMHCm"));
     }
 
 }
