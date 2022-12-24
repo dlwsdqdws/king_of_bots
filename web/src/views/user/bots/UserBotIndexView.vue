b3e3b6efd552122ebd25b2561d719006d4ce7630
dlwsdqdws <dlwsdqdws@gmail.com>
2022-12-25 03:07:21 +0800

    front_getlist

--- web/src/views/user/bots/UserBotIndexView.vue
+++ web/src/views/user/bots/UserBotIndexView.vue
@@ -1,18 +1,61 @@
 <template>
-    <ContentField>my bots</ContentField>
+    <div class="container">
+        <div class="row">
+            <div class="col-3">
+                <div class="card" style = "margin-top: 20px;">
+                    <div class="card-body">
+                        <img :src = "$store.state.user.photo" alt = "" style = "width:100%">
+                    </div>
+                </div>
+            </div>
+            <div class="col-9">
+                <div class="card" style = "margin-top: 20px;">
+                    <div class="card-header">
+                        <span style = "font-size: 150%;">
+                            My Bots
+                        </span>
+                        <button type="button" class="btn btn-primary float-end">
+                            Create A Bot
+                        </button>
+                    </div>
+                    <div class = "card-body">
+                        <table class="table table-hover">
+                            <thead>
+                                <tr>
+                                    <th>Name</th>
+                                    <th>Create Time</th>
+                                    <th>Operations</th>
+                                </tr>
+                            </thead>
+                            <tbody style = "font-size: 90%;">
+                                <tr v-for = "bot in bots" :key = "bot.id">
+                                    <td>{{ bot.title }}</td>
+                                    <td>{{ bot.createtime }}</td>
+                                    <td>
+                                        <button type="button" class="btn btn-secondary" style = "margin-right: 10px;">Modify</button>
+                                        <button type="button" class="btn btn-danger">Delete</button>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-    import ContentField from "../../../components/ContentField.vue";
     import $ from "jquery";
     import {useStore} from "vuex";
+    import {ref} from "vue";
 
     export default {
-        components : {
-            ContentField
-        },
         setup() {
             const store = useStore();
+            let bots = ref([]);
+
+            // test APIs
             // $.ajax({
             //     url : "http://127.0.0.1:3000/user/bot/add/",
             //     type : "post",
@@ -69,19 +112,38 @@
             //     }
             // });
 
-            $.ajax({
-                url : "http://127.0.0.1:3000/user/bot/getlist/",
-                type : "get",
-                headers : {
-                    Authorization : "Bearer " + store.state.user.token,
-                },
-                success(resp){
-                    console.log(resp);
-                },
-                error(resp){
-                    console.log(resp);
-                }
-            });
+            // $.ajax({
+            //     url : "http://127.0.0.1:3000/user/bot/getlist/",
+            //     type : "get",
+            //     headers : {
+            //         Authorization : "Bearer " + store.state.user.token,
+            //     },
+            //     success(resp){
+            //         console.log(resp);
+            //     },
+            //     error(resp){
+            //         console.log(resp);
+            //     }
+            // });
+
+            const refresh_bots = () => {
+                $.ajax({
+                    url : "http://127.0.0.1:3000/user/bot/getlist/",
+                    type : "get",
+                    headers : {
+                        Authorization : "Bearer " + store.state.user.token,
+                    },
+                    success(resp){
+                        bots.value = resp;
+                    }
+                });
+            }
+
+            refresh_bots();
+
+            return {
+                bots
+            }
         }
     }
 </script>
