4d47d7d67e64340a3465de5eaf04126048ec5931
dlwsdqdws <dlwsdqdws@gmail.com>
2022-12-30 03:25:38 +0800

    record_video

--- web/src/views/pk/PKIndexView.vue
+++ web/src/views/pk/PKIndexView.vue
@@ -22,6 +22,8 @@
             const store = useStore();
             const socketUrl = `ws://127.0.0.1:3000/websocket/${store.state.user.token}/`;
 
+            store.commit("updateIsRecord", false);
+
             let socket = null;
 
             store.commit("updateOpponent", {
--- web/src/views/record/RecordIndexView.vue
+++ web/src/views/record/RecordIndexView.vue
@@ -1,6 +1,6 @@
 <template>
     <ContentField>
-        <table class="table table-striped table-hover">
+        <table class="table table-striped table-hover" style = "text-align: center;">
             <thead>
                 <tr>
                     <th>PlayerA</th>
@@ -31,7 +31,7 @@
                         {{ record.record.createtime }}
                     </td>
                     <td>
-                        <button type="button" class="btn btn-secondary">View Steps</button>
+                        <button  @click="open_record_content(record.record.id)" type="button" class="btn btn-secondary">View Steps</button>
                     </td>
                 </tr>
             </tbody>
@@ -44,6 +44,7 @@
     import { useStore } from "vuex";
     import { ref } from "vue";
     import $ from 'jquery';
+    import router from "../../router/index";
 
     export default {
         components : {
@@ -82,8 +83,53 @@
 
             console.log(tot_records);
 
+            const stringTo2D = map => {
+                let g = [];
+                for(let i = 0, k = 0; i < 13; i++){
+                    let line = [];
+                    for(let j = 0; j < 14; j++, k++){
+                        if(map[k] === '0') line.push(0);
+                        else line.push(1);
+                    }
+                    g.push(line);
+                }
+
+                return g;
+            }
+
+            const open_record_content = recordId => {
+                for(const record of records.value){
+                    if(record.record.id === recordId){
+                        store.commit("updateIsRecord", true);
+                        // console.log(record);
+                        store.commit("updateGame", {
+                            map : stringTo2D(record.record.map),
+                            a_id : record.record.aid,
+                            a_sx : record.record.asx,
+                            a_sy : record.record.asy,
+                            b_id : record.record.bid,
+                            b_sx : record.record.bsx,
+                            b_sy : record.record.bsy,
+                        });
+                        store.commit("updateSteps", {
+                            a_steps : record.record.asteps,
+                            b_steps : record.record.bsteps,
+                        });
+                        store.commit("updateRecordLoser", record.record.loser);
+                        router.push({
+                            name : "record_content",
+                            params : {
+                                recordId,
+                            }
+                        });
+                        break;
+                    }
+                }
+            }
+
             return {
                 records,
+                open_record_content,
             }
         }
     }
